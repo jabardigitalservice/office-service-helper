ff2891d69aa5fdd07f14188f7f289a4f8446a0d7
Fajar Hikmal <fajarhikmal214@gmail.com>
2023-02-28 11:27:52 +0700

    refactor(generate-pdf): Merge generate pdf method into sign method

--- src/internal/pdf-generations/usecase/usecase.ts
+++ src/internal/pdf-generations/usecase/usecase.ts
@@ -1,18 +1,11 @@
-import { BAD_REQUEST } from 'http-status'
-import puppeteer, { Browser } from 'puppeteer'
-import { Config } from '../../../config/config.interface'
-import MinioClient from '../../../external/storage/minio'
-import createFileObject from '../../../helpers/createFileObject'
+import { Browser } from 'puppeteer'
 import error from '../../../pkg/error'
 import statusCode from '../../../pkg/statusCode'
 
-class UseCase {
-    public minioClient: MinioClient
-    constructor(public config: Config, public browser: Browser | any) {
-        this.minioClient = new MinioClient(config)
-    }
+class PdfGenerateUsecase {
+    constructor(private browser: Browser) {}
 
-    public async generatePdf(url: string) {
+    public async GeneratePdf(url: string): Promise<Buffer> {
         try {
             const page = await this.browser.newPage()
             await page.goto(url, {
@@ -21,26 +14,11 @@ class UseCase {
             const pdf = await page.pdf({ format: 'A4' })
             await this.browser.close()
 
-            const { fileName, fileBuffer, metaData } = createFileObject(pdf)
-            const upload = await this.minioClient.minio.putObject(
-                this.config.minio.bucketName,
-                fileName,
-                fileBuffer,
-                metaData
-            )
-
-            const file_url = `${this.config.core_api.url}/files/${fileName}`
-
-            return {
-                data: {
-                    fileName,
-                    file_url,
-                },
-            }
+            return pdf
         } catch (err) {
-            return new error(statusCode.BAD_REQUEST, JSON.stringify(err))
+            throw new error(statusCode.BAD_REQUEST, JSON.stringify(err))
         }
     }
 }
 
-export default UseCase
+export default PdfGenerateUsecase
