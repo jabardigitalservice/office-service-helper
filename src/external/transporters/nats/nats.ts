645d2e392dc52966aa103605cfa102aa27ee155b
Fajar Hikmal <fajarhikmal214@gmail.com>
2023-03-06 08:20:58 +0700

    feat(generate-pdf): Setup nats and setting up generate-pdf service for consumption  by esigns

---
+++ src/external/transporters/nats/nats.ts
@@ -0,0 +1,21 @@
+import winston from 'winston'
+import { connect, ConnectionOptions, NatsConnection } from 'nats'
+import { Config } from '../../../config/config.interface'
+
+class Nats {
+    public async connect(
+        config: Config,
+        logger: winston.Logger
+    ): Promise<NatsConnection> {
+        const server: ConnectionOptions = {
+            servers: config.nats.url,
+        }
+
+        const client = await connect(server)
+        logger.info(`Nats Client Connected to ${client.getServer()}`)
+
+        return client
+    }
+}
+
+export default Nats
--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,14 @@
 import config from './config/config'
 import Logger from './pkg/logger'
-import Redis from './pkg/redis'
 import Http from './transport/http/http'
-import puppeteer from 'puppeteer'
 import Esigns from './internal/esigns/esigns'
 
 const main = async () => {
     const { logger } = new Logger(config)
-    const redis = new Redis(config, logger)
     const http = new Http(logger, config)
-    const browser = await puppeteer.launch({ headless: true })
 
     // Load internal apps
-    new Esigns(http, logger, config, browser)
+    new Esigns(logger, config)
 
     if (config.app.env !== 'test') {
         http.Run(config.app.port.http)
